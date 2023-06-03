70cc986d44bd39a17a52d2431c54de2568c9d718
Kmnarvaezmorales <kelvin.narvaez@yahoo.com>
2023-06-02 21:42:00 -0400

    Added Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
