15b464d4b66aeb3f446e6676d9dd6f26f917c766
Kmnarvaezmorales <kelvin.narvaez@yahoo.com>
2023-06-04 23:34:55 -0400

    Data Binded Cards and Used ngForLoop to add cards

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
-export class CardsComponent {
 
+export class CardsComponent {
+  @Input() img: string;
+  @Input() title: string;
+  @Input() description: string;
+
+  constructor(){
+    this.img = "";
+    this.title = "";
+    this.description = "";
+  }
 }
---
+++ src/app/cards/mock_product_item.ts
@@ -0,0 +1,34 @@
+export const mock_product_list = [
+    {
+        img: "./assets/NK_StarWars_Tatooine_tile_2048x.webp",
+        title: "SA Tatooine $250",
+        description: "ABS Double-Shot Keycaps"
+    },
+    {
+        img: "./assets/PBT_Olivia_Tile_2048x.webp",
+        title: "Cherry Olivia $95",
+        description: "PBT Dyesub Keycaps"
+    },
+    {
+        img: "./assets/Spellbook_16x9-Tile_2048x.webp",
+        title: "Cherry Spellbook $95",
+        description: "PBT Dyesub Keycaps"
+    }
+    
+    // {
+    //     img: "./assets/NK_Cream_bb0aa86b-b4ba-4151-99c9-0784e7242d00_2048x.webp",
+    //     title: "Cream+ Switches",
+    //     description: "36 Per Box"
+    // },
+    // {
+    //     img: "./assets/NK_Cream_Clickie_2048x.webp",
+    //     title: "Cream Clickie Switches",
+    //     description: "36 Per Box"
+    // },
+    // {
+    //     img: "./assets/Box75_35fa1776-71e1-4f8a-8899-ed0b6aed5d6c_2048x.webp",
+    //     title: "Box 75 Keyboard",
+    //     description: ""
+    // }
+    
+];
